6294e2d5bc140e398c72b255f1449065eabaebc8
MerlynnDust <anthony.angel@x2i.systems>
2018-11-06 04:18:10 -0500

    vcs xml file

---
+++ settings.py
@@ -0,0 +1,13 @@
+""" Settings
+This file will try to automate where the source comes from, so editing it should not be necessary, but for now it will be hard coded
+
+function that will take user input, search for the book they want on google and do its best to find the story online.
+
+"""
+
+class settings:
+    #Booksites
+    Novel12 = "https://novel12.com/"
+    Gutenberg = "https://www.gutenberg.org/"
+
+    source = Novel12
